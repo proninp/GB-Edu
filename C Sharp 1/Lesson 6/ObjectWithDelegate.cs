01a8124b7f59af143e90f51e4f0a8d0b5b9dfa14
Pavel <proninpaul@yandex.ru>
2021-04-06 09:24:38 +0300

    Lesson 06

---
+++ C Sharp 1/Lesson 6/ObjectWithDelegate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lesson_6
+{
+    
+    class ObjectWithDelegate
+    {
+        double x, y;
+        public double X { get => x; set => x = value; }
+        public double Y { get => y; set => y = value; }
+        public delegate double Fun(double x, double y);
+        public Fun Del { get; set; }
+
+        public ObjectWithDelegate(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public ObjectWithDelegate(double x, double y, Fun del): this(x, y) => this.Del = del;
+
+        public void Table(double c)
+        {
+            Console.WriteLine("----- X ----- Y -----");
+            while (x <= y)
+                Console.WriteLine("| {0,8:0.000} | {1,8:0.000} |", x, Del(x++, c));
+            Console.WriteLine("---------------------");
+        }
+    }
+}
---
+++ C Sharp 1/Lesson 6/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lesson_6
+{
+    class Student
+    {
+        static int bakalavr;
+        static int magistr;
+
+        string lastName;
+        string firstName;
+        string university;
+        string faculty;
+        int course;
+        string department;
+        int group;
+        string city;
+        int age;
+
+        public string LastName { get => lastName; set => lastName = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string University { get => university; set => university = value; }
+        public string Faculty { get => faculty; set => faculty = value; }
+        public int Course { get => course; set => course = value; }
+        public string Department { get => department; set => department = value; }
+        public int Group { get => group; set => group = value; }
+        public string City { get => city; set => city = value; }
+        public int Age { get => age; set => age = value; }
+        public static int Bakalavr { get => bakalavr; set => bakalavr = value; }
+        public static int Magistr { get => magistr; set => magistr = value; }
+
+        public Student(string lastName, string firstName, string univercity, string faculty, int course, string department, int group, string city, int age)
+        {
+            this.lastName = lastName;
+            this.firstName = firstName;
+            this.university = univercity;
+            this.faculty = faculty;
+            this.course = course;
+            this.department = department;
+            this.group = group;
+            this.city = city;
+            this.age = age;
+            if (course < 5) bakalavr++; else magistr++;
+        }
+    }
+}
