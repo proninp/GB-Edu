6f39db4c3676f96b9cd0861acbbffd823e7be025
Pavel <proninpaul@yandex.ru>
2021-05-21 13:48:38 +0300

    Java 1 Lesson 08

---
+++ Java 1/src/main/java/ru/geekbrains/lesson08/swing/MainApp.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.lesson08.swing;
+
+public class MainApp {
+    // Homework:
+    // 1. Understand the code of the game " Guess the number"
+    // 2. In the game" Guess the number " make a button to restart the game:
+    // the application must guess a new number, and write to the user
+    // that the game has started
+    // 3. * Try to add 3 attempts for guessing
+
+    public static void main(String[] args) {
+        new GameWindow();
+    }
+}
